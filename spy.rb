81f784332704a59280bf8ca62b67c5ed121816e9
gitmangoes <git@mangoes>
2012-09-14 00:13:07 -0700

    Revised commit message

--- spy.rb
+++ spy.rb
@@ -1,6 +1,7 @@
 # Create a "Spy" object which will accept any messages passed to it, but not reveal any information about itself
 # Introduction to method_missing method
 
+
 class Spy
   def method_missing(sym, *args)
   end
