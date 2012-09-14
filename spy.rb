a072fbf40d53e575fe005aa830be67228152a8b2
gitmangoes <git@mangoes>
2012-09-14 00:07:34 -0700

    Introduce #method_missing

---
+++ spy.rb
@@ -0,0 +1,10 @@
+# Create a "Spy" object which will accept any messages passed to it, but not reveal any information about itself
+# Introduction to method_missing method
+
+class Spy
+  def method_missing(sym, *args)
+  end
+end
+
+Spy.new.hello
+Spy.new.goodbye
